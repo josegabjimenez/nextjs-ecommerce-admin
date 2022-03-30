08f21448914be4cda331074ce8b0afe199db8d9b
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-03-30 14:03:03 -0500

    :construction: [WORKING] Add product modal
    Image validation was added

--- src/components/AddProductModal.jsx
+++ src/components/AddProductModal.jsx
@@ -19,6 +19,8 @@ const AddProductModal = () => {
     const fullData = {
       ...data,
       price: parseInt(data.price),
+      categoryId: parseInt(data.categoryId),
+      images: [data.images[0].name],
     };
     console.log(fullData);
   };
@@ -37,8 +39,8 @@ const AddProductModal = () => {
             </span>
             {/* Input */}
             <input
-              id="title"
               {...register('title', { required: { value: true, message: 'El campo es requerido' } })}
+              id="title"
               type="text"
               placeholder="Título del producto"
               className={`input input-bordered input-sm ${errors.title ? 'border-red-500' : ''}`}
@@ -93,8 +95,8 @@ const AddProductModal = () => {
               Categoria
             </span>
             {/* Input */}
-            <select className="select select-bordered font-normal select-sm" {...register('category', { required: { value: true, message: 'El campo es requerido' } })}>
-              <option disabled selected className="">
+            <select {...register('categoryId', { required: { value: true, message: 'El campo es requerido' } })} className="select select-bordered font-normal select-sm">
+              <option disabled selected className="text-gray-400">
                 Elige una categoria
               </option>
               {categories?.map((category) => (
@@ -107,7 +109,7 @@ const AddProductModal = () => {
           {errors?.category?.type === 'required' && <WarningAlert className="text-xs mt-1 rounded-md">{errors.category.message}</WarningAlert>}
         </div>
 
-        {/* Product category section */}
+        {/* Product Description section */}
         <label className="input-group input-group-vertical col-span-2">
           <span className="font-bold">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -115,25 +117,30 @@ const AddProductModal = () => {
             </svg>
             Descripción
           </span>
-          <textarea className="textarea textarea-bordered" placeholder="Descripción del producto" />
+          <textarea {...register('description')} className="textarea textarea-bordered" placeholder="Descripción del producto" />
         </label>
 
         {/* Upload product image section */}
-        <label className="input-group input-group-vertical col-span-2 ">
-          <span className="font-bold">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-            </svg>
-            Upload file
-          </span>
-          <label
-            htmlFor="image"
-            className="px-4 py-2 block w-full text-sm rounded-lg rounded-t-none border border-gray-300 cursor-pointer text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-200"
-          >
-            Elige una imagen
+        <div className="col-span-2">
+          <label className="input-group input-group-vertical">
+            <span className="font-bold">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+              </svg>
+              Upload file
+            </span>
+            <label
+              htmlFor="image"
+              className={`px-4 py-2 block w-full text-sm rounded-lg rounded-t-none border border-gray-300 cursor-pointer text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-200 ${
+                errors.images ? 'ring-red-500 ring-1' : ''
+              }`}
+            >
+              Elige una imagen
+            </label>
+            <input {...register('images', { required: { value: true, message: 'El campo es requerido' } })} placeholder="Subir la imagen del producto" className="hidden" id="image" type="file" />
           </label>
-          <input placeholder="Subir la imagen del producto" className="hidden" id="image" name="image" type="file" />
-        </label>
+          {errors?.images?.type === 'required' && <WarningAlert className="text-xs mt-1 rounded-md">{errors.images.message}</WarningAlert>}
+        </div>
 
         <div className="w-full no-animation flex justify-end col-span-2">
           {/* Modal action buttons */}
